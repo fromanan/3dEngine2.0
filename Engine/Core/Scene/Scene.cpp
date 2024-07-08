3dc95a99ed9e994c4aa52ef9af3c8e03a79c0977
Hayden <kokokokolol10@gmail.com>
2024-07-08 10:28:26 -0400

    Textures can not be fully transparent

--- Engine/Core/Scene/Scene.cpp
+++ Engine/Core/Scene/Scene.cpp
@@ -69,7 +69,7 @@ void Scene::RenderObjects() {
 	
 	GLuint programid = Renderer::GetCurrentProgramID();
 	glEnable(GL_BLEND);
-	glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
 	for (int i = 0; i < AssetManager::GetAllGameObjects().size(); i++) {
 		glm::mat4 ModelMatrix = AssetManager::GetGameObject(i)->GetModelMatrix();
