c467db3ff1ab9a1489a2f881520e128c3077546e
Tony Froman <33915703+fromanan@users.noreply.github.com>
2024-08-01 19:45:29 -0400

    Removed redundant constant declaration (already in header file)

--- Engine/Loaders/loader.cpp
+++ Engine/Loaders/loader.cpp
@@ -67,10 +67,6 @@ namespace loader {
 		return textureID;
 	}
 
-#define FOURCC_DXT1 0x31545844 // Equivalent to "DXT1" in ASCII
-#define FOURCC_DXT3 0x33545844 // Equivalent to "DXT3" in ASCII
-#define FOURCC_DXT5 0x35545844 // Equivalent to "DXT5" in ASCII
-
 	GLuint loader::loadDDS(const char* imagepath) {
 		unsigned char header[124];
 
