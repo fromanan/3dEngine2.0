3e01b1a0a9f70d6391396e4c090228d3bb58c30b
Tony Froman <33915703+fromanan@users.noreply.github.com>
2024-08-01 19:48:26 -0400

    Moved constant declarations outside of namespace (not necessary for .h)

--- Engine/Loaders/loader.hpp
+++ Engine/Loaders/loader.hpp
@@ -13,19 +13,12 @@
 #include <fstream>
 #include <iostream>
 
-
-
-
-
-namespace loader {
-
-
-	GLuint loadBMP_custom(const char* imagepath);
-
 #define FOURCC_DXT1 0x31545844 // Equivalent to "DXT1" in ASCII
 #define FOURCC_DXT3 0x33545844 // Equivalent to "DXT3" in ASCII
 #define FOURCC_DXT5 0x35545844 // Equivalent to "DXT5" in ASCII
 
+namespace loader {
+	GLuint loadBMP_custom(const char* imagepath);
 	GLuint loadDDS(const char* imagepath);
 	bool loadOBJ(const char* path, std::vector<glm::vec3>& out_vertices, std::vector<glm::vec2>& out_uvs, std::vector<glm::vec3>& out_normals);
 }
