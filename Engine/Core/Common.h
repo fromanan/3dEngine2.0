5b24a5fc9607610a1ac34dffc01ef81638807803
Tony Froman <33915703+fromanan@users.noreply.github.com>
2024-08-02 16:57:08 -0400

    Moved Transform to its own file

--- Engine/Core/Common.h
+++ Engine/Core/Common.h
@@ -5,6 +5,7 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include "bullet/btBulletDynamicsCommon.h"
+#include "Transform.h"
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
@@ -15,24 +16,6 @@
 #define SCREENWIDTH 1024
 #define SCREENHEIGHT 768
 #define WINDOWTITILE "Engine 2.0"
-
-struct Transform
-{
-    glm::vec3 position = glm::vec3(0);
-    glm::vec3 rotation = glm::vec3(0);
-    glm::vec3 scale = glm::vec3(1);
-
-    glm::vec3 getPosition() { return position; }
-    glm::vec3 getRotation() { return rotation; }
-    glm::vec3 getScale() { return scale; }
-
-    glm::mat4 to_mat4() {
-        glm::mat4 m = glm::translate(glm::mat4(1), position);
-        m *= glm::mat4_cast(glm::quat(rotation));
-        m = glm::scale(m, scale);
-        return m;
-    }
-};
 
 btVector3 glmToBtVector3(const glm::vec3& vec);
 
---
+++ Engine/Core/Transform.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include <glm/fwd.hpp>
+#include <glm/ext/matrix_transform.hpp>
+#include <glm/gtc/quaternion.hpp>
+
+struct Transform
+{
+    glm::vec3 position = glm::vec3(0);
+    glm::vec3 rotation = glm::vec3(0);
+    glm::vec3 scale = glm::vec3(1);
+
+    glm::vec3 getPosition() const { return position; }
+    glm::vec3 getRotation() const { return rotation; }
+    glm::vec3 getScale() const { return scale; }
+
+    glm::mat4 to_mat4() const {
+        glm::mat4 m = glm::translate(glm::mat4(1), position);
+        m *= glm::mat4_cast(glm::quat(rotation));
+        m = glm::scale(m, scale);
+        return m;
+    }
+};
