51fe1142375281172e69d35ddafd64b3cef024a4
Tony Froman <33915703+fromanan@users.noreply.github.com>
2024-08-02 23:03:01 -0400

    Added pch file

---
+++ Engine/pch.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include <iostream>
+#include <memory>
+#include <utility>
+#include <algorithm>
+#include <functional>
+
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <vector>
+#include <map>
+#include <set>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/glm.hpp>
+
+#include "Common.h"
+#include "GameObject.h"
+
+#ifdef SW_PLATFORM_WINDOWS
+    #include <Windows.h>
+#endif
